c3e35b49b16a0994856d8654d11da6b18bda88cd
SmartConTractor <login.blockchain888@gmail.com>
2025-03-19 15:02:46 +0100

    Extract desired information from the web.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# work with urls
-import requests 
-# extract only specific information from the source code
-import selectorlib 
+import requests # work with urls
+
+import selectorlib # extract only specific information from the source code
 
 
 url = "http://programmer100.pythonanywhere.com/tours/"
+
+id="displaytimer" # id code for the desired part
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -12,6 +13,17 @@ def scrape(url):
     source = response.text
     return(source)
 
+
+def extract(source):
+    """"""
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    
+    value = extractor.extract(source)["tours"] # return the key "tours" from the dictionary 
+    return value
+
+
 if __name__ == "__main__":
-    print(scrape(url))
+    scraped = scrape(url)
+    extracted = extract(scraped)
+    print(extracted)
 
